c0511e6da6a1e8238a5a0f7369b0f9d6ece52896
katherina17 <katherinepetrova76@gmail.com>
2023-01-29 21:07:10 +0300

    feat: add useMemo for select

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useMemo } from 'react';
 import './App.css';
 import {RatingSecret, RatingValuePropsType, valueType} from "./components/Rating/RatingSecret";
 import {OnOff} from "./components/OnOff/OnOf";
@@ -8,6 +8,7 @@ import {SelectSecret, SelectType} from "./components/Select/SelectSecret";
 import {UnctrolledAccordionSecret} from "./components/UnconrolledAccordion/UncontrolledAccordion";
 import {Example1} from "./components/ReactMemo/ReactMemo";
 import AccordionSecret from "./components/Accordion/Accordion";
+import {ExampleSelectWithMemoSecret} from "./components/ExampleSelectWithMemo/ExampleSelectWithMemoSecret";
 
 const usersValues = [
     {id: 1, value: 'Stas'},
@@ -34,12 +35,45 @@ const selectData: SelectType[] = [
 
 ]
 
+export type SelectWithMemo = {
+    id: number;
+    value: string;
+    title: string;
+    citizens?: number;
+}
+
+const DataForExampleSelectWithMemo: SelectWithMemo[] = [
+    {id: 1, value: '1', title: 'none'},
+    {id: 2, value: '2', title: 'Minsk', citizens: 50000},
+    {id: 3, value: '3', title: 'Moscow', citizens: 100000},
+    {id: 4, value: '4', title: 'Gomel', citizens: 30000},
+
+]
+
 function App() {
     const[ratingValue, setRatingValue] = useState<valueType>(0);
     const[collapsedValue, setCollapsedValue] = useState(false);
     const [light, setLight] = useState<boolean>(false);
     const [selectValue, setSelectValue] = useState('1');
+    const [selectValueWithO, setSelectValueWithO] = useState('1');
+    const [selectValueCitizen, setSelectValueCitizen] = useState('1');
 
+    const usualSelectData = useMemo(()=> {
+        return DataForExampleSelectWithMemo;
+    }, [])
+    const selectDataWithOMemo = useMemo(()=> {
+        return DataForExampleSelectWithMemo.filter(el => el.title.indexOf('o') !== -1);
+    }, []);
+    const selectDataCitizenMemo = useMemo(()=> {
+        return DataForExampleSelectWithMemo.filter(el => el.citizens ? el.citizens > 30000 : el);
+    }, [])
+   /* const [selectValue, setSelectValue] = useState<SelectWithMemo[]>([
+        {id: 1, value: '1', title: 'none'},
+        {id: 2, value: '2', title: 'Minsk', citizens: 50000},
+        {id: 3, value: '3', title: 'Moscow', citizens: 100000},
+        {id: 4, value: '4', title: 'Gomel', citizens: 30000},
+
+    ])*/
 
     const onClickHandler = (value: string) => {
         console.log(`Hello, ${value}, have a good day!`)
@@ -63,11 +97,14 @@ function App() {
                        setCollapsedValue={setCollapsedValue}
                        callBack={(value) => onClickHandler(value)}/>
 
-            <Select selectData={selectData}
+          {/*  <Select selectData={selectData}
                     setSelectValue={setSelectValue}
-                    value={selectValue}/>
+                    value={selectValue}/>*/}
             <UncontrolledAccordion title={'users'}/>
             <Example1/>
+            <ExampleSelectWithMemo selectData={usualSelectData} value={selectValue} setSelectValue={setSelectValue}/>
+            <ExampleSelectWithMemo selectData={selectDataWithOMemo} value={selectValueWithO} setSelectValue={setSelectValueWithO}/>
+            <ExampleSelectWithMemo selectData={selectDataCitizenMemo} value={selectValueCitizen} setSelectValue={setSelectValueCitizen}/>
         </div>
     );
 }
@@ -78,6 +115,6 @@ const Select = React.memo(SelectSecret)
 const UncontrolledAccordion= React.memo(UnctrolledAccordionSecret)
 const UncontrolledInputs= React.memo(UncontrolledInputsSecret)
 const UncontrolledOnOff= React.memo(UncontrolledOnOffSecret)
-
+const ExampleSelectWithMemo= React.memo(ExampleSelectWithMemoSecret)
 
 export default App;
---
+++ src/components/ExampleSelectWithMemo/ExampleSelectWithMemoSecret.tsx
@@ -0,0 +1,83 @@
+import {SelectWithMemo} from "../../App";
+import React, {KeyboardEvent, useState} from "react";
+import s from "../Select/Select.module.css";
+
+type ExampleSelectWithMemoPropsType = {
+    selectData: SelectWithMemo[]
+    value: string;
+    setSelectValue: (value: string) => void;
+}
+
+export const ExampleSelectWithMemoSecret = (props: ExampleSelectWithMemoPropsType) => {
+    let currValue = props.selectData.find(el => el.value === props.value);
+    const [isSelectedValue, setIsSelectedValue] = useState(false);
+    const onKeyUpHandler = (e: KeyboardEvent<HTMLDivElement>) => {
+        for(let i = 0; i < props.selectData.length; i++){
+            if(e.keyCode === 40 && (currValue && currValue.value) === props.selectData[i].value){
+                if(i === props.selectData.length - 1) break
+                props.setSelectValue(props.selectData[i+1].value)
+                break;
+            } else if(e.keyCode === 38 && (currValue && currValue.value) === props.selectData[i].value){
+                if(i === 0) break;
+                props.setSelectValue(props.selectData[i-1].value)
+                break;
+            }
+            if(e.code === 'Enter'){
+                setIsSelectedValue(false)
+            }
+        }
+    }
+    const onClickHandler = () => {
+        setIsSelectedValue(!isSelectedValue)
+    }
+    const changeInputValueOnClick = (value: string) => {
+        props.setSelectValue(value);
+    }
+    return (
+        <>
+            <div className={isSelectedValue ? `${s.selectActive}` : s.select} onClick={onClickHandler} onKeyUp={onKeyUpHandler} tabIndex={0}>
+                <span>{currValue && currValue.title}</span>
+                <div className={ isSelectedValue ? s.arrowActive : s.arrow}></div>
+            </div>
+            <div>
+                {isSelectedValue && <ModalSelectValues selectData={props.selectData}
+                                                       callBack={changeInputValueOnClick}
+                                                       selectedValue={currValue && currValue.value}
+                                                       isSelectedValue={isSelectedValue}
+                                                       setIsSelectedValue={setIsSelectedValue}
+                />}
+            </div>
+        </>
+    )
+}
+
+type ModalSelectValues = {
+    selectData: SelectWithMemo[]
+    callBack: (title: string) => void
+    selectedValue: string | undefined;
+    isSelectedValue: boolean;
+    setIsSelectedValue: (val: boolean) => void
+}
+
+const ModalSelectValuesSecret = (props: ModalSelectValues) => {
+    const addCallBacks = (value: string) => {
+        props.callBack(value);
+        props.setIsSelectedValue(!props.isSelectedValue)
+    }
+    return (
+        <div className={s.modalSelectValues}>
+            {props.selectData.map(el => {
+                    return (
+                        <div className={props.selectedValue === el.value ? s.targetItem : ''}
+                             key={el.id}
+                             onClick={() => addCallBacks(el.value)}
+                        >{el.title}</div>
+                    )
+                }
+            )
+            }
+        </div>
+    )
+}
+
+const ModalSelectValues = React.memo(ModalSelectValuesSecret);
